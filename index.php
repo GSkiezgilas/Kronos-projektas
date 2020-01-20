9fb916e456f8832d42a9d456c633593d0bf95807
GSkiezgilas <giedriusskiezgilas@yahoo.com>
2020-01-20 16:21:31 +0200

    index, header and footer files added

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Kūrybingas ir profesionalus vestuvių fotografas Vilniuje, Kaune, Klaipėdoje ir Visoje Lietuvoje. Priešvestuvinės fotosesijos, fotoknygos, paveikslai ant drobės">
+    <meta name="keywords" content="vestuvių fotografas, fotosesija, nuotraukos, fotoknyga">
+    <meta name="author" content="Giedrius Skiezgilas">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagrindinis | Kronos portraits</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <?php include 'pages/header.html'; ?>
+    <main>
+        <h1>Galerija</h1>
+        <div class="main-gallery-links-container">
+            <section class="gallery-photolink-container"><a href="/pages/vestuves.php">
+                <h3 class="landing_page">Vestuvės</h3>
+                <img src="images/homepage/vestuves.JPG" alt="Vestuvių akimirka"></a>
+            </section>
+            <section class="gallery-photolink-container"><a href="/pages/portretai.php">
+                <h3 class="landing_page">Portretai</h3>
+                <img src="images/homepage/portretai.JPG" alt="Merginos portretas"></a>
+            </section>
+            <section class="gallery-photolink-container"><a href="/pages/vaikai.php">
+                <h3 class="landing_page">Vaikai</h3>
+                <img src="images/homepage/vaikai.jpg" alt="Vaikai pievoje"></a>
+            </section>
+        </div>
+    </main>
+    <?php include 'pages/footer.html'; ?>
+</body>
+</html>
