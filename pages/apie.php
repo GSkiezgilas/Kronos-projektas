f3246c65039db17c7f689b3594f8189531c20049
GSkiezgilas <giedriusskiezgilas@yahoo.com>
2020-01-21 01:59:57 +0200

    atsiliepimai ir apie pages added

---
+++ pages/apie.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apie | Kronos Portraits</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <?php include 'header.html'; ?>
+    <main>
+        <h1>Apie</h1>
+        <article class="apie-main-container">
+            <img src="/images/apie/as.jpg" alt="Vyro portretas">
+            <p>Sveiki,<br>aš vardu Giedrius, Kronos Portraits fotografas. Džiaugiuosi, jog užsukote į mano puslapį.</p>
+            <p>Fotografija rimčiau susidomėjau, kada grįžus iš kelionių ir žiūrint kelionių nuotraukas supratau, jog jos neperteikia matytų miestų dydybės, žmonių įvairovės ar gamtos spalvingumo. Žingsnis po žingsnio besigilinant į fotografiją atėjo supratimas, kad mano nuotraukos gali džiuginti ir daugiau žmonių (jų atsiliepimus galite pamatyti <a href="atsiliepimai.php">čia</a>), tad 2013 metais prasidėjo mano kaip fotografo karjera.</p>
+            <p>Laisvu nuo fotografijos laiku mane galite sutikti vandenlenčių parke, golfo aikštyne, sporto salėje ar užmetusį meškerę prie kokio vandens telkinio. Kad ir kur tai bebūtų, drąsiai prieikit pasilabinti, aš visada džiaugiuosi naujomis pažintimis.</p>
+            <p>Jeigu jau perskaitėte iki šios vietos, bet dar nematėte mano darbų, juos galite pamatyti <a href="../index.php">čia</a>, į rūpimą klausimą atsakymą greičiausiai rasite <abbr title="Dažniausiai užduodami klausimai">D.U.K.</abbr> skyrelyje <a href="duk.php">čia</a>, o jei apsisprendėte ir norite užsisakyti mano paslaugas, tai padaryti galite <a href="kontaktai.php">čia</a>.</p>
+        </article>
+    </main>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
---
+++ pages/atsiliepimai.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atsiliepimai | Kronos Portraits</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <?php include 'header.html'; ?>
+    <div class="main-container">
+        <h1>Atsiliepimai</h1>
+        <main>
+            <div class="testimonial-container">
+              <img src="../images/atsiliepimai/AK.jpg" alt="A">
+              <p><b>Agnė ir Karolis</b></p>
+              <p>Nuotraukos pateisino visus lūkesčius su kaupu, super! 😊 Esam labai patenkinti ir visiems rekomenduojam.</p>
+            </div>
+            <div class="testimonial-container">
+              <img src="../images/atsiliepimai/GP.jpg" alt="B">
+              <p><b>Gintarė ir Paulius</b></p>
+              <p>Tik pačios geriausios rekomendacijos!  Labai džiaugiamės patikėję svarbiausią mūsų dieną Giedriui 😊 Nuotraukos viena už kitą gražesnės. Ačiū!</p>
+            </div>
+            <div class="testimonial-container">
+              <img src="../images/atsiliepimai/RG.jpg" alt="B">
+              <p><b>Renata</b></p>
+              <p>Pirmą kartą teko pozuoti prieš objektyvą, bet fotografo profesionalumas, šiltumas, supratimas tai pavertė smagia pramoga ir neapsakoma patirtimi. Fotosesijos lengvumas, o svarbiausia dėmesys žmogui, padėjo atsipalaiduoti fotosesijos metu.<br>AČIŪ už patarimus ir gerą nuotaiką. AČIŪ už įamžintas akimirkas, nuostabias fotografijas ir dovanas. SĖKMĖS!</p>
+            </div>
+            <div class="testimonial-container">
+              <img src="../images/atsiliepimai/KT.jpg" alt="B">
+              <p><b>Kamilė</b></p>
+              <p>Profesionalus fotografas, puikiai išmano savo darbą. Kartu dirbti buvo labai maloni patirtis, esu labai patenkinta asmenine fotosesija, o portretai tiesiog nuostabūs. Labai Jums ačiū! Tikrai rekomenduoju!</p>
+            </div>
+            <div class="testimonial-container">
+              <img src="../images/atsiliepimai/DV.jpg" alt="B">
+              <p><b>Dovilė ir Vaidas</b></p>
+              <p>Esame labai patenkinti Giedriaus darbu. Nuotraukos superinės, perteikia nuostabią nuotaiką, kuri mus lydėjo tą dieną 😊 Mums kaip fotografavimosi naujokams buvo smagu, ir nejautėme jokio diskomforto prieš objektyvą 😁 Su Giedriaus pagalba viskas išėjo labai natūraliai ir smagiai 😊 Ačiū 😊</p>
+            </div>
+        </main>
+    </div>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
