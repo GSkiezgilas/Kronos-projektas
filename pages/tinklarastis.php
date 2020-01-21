ff7bc49f90423b0d505b2d9eaa3ee083716177b7
GSkiezgilas <giedriusskiezgilas@yahoo.com>
2020-01-21 12:59:14 +0200

    main tinklarastis page added

---
+++ pages/tinklarastis.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tinklaraštis | Kronos Portraits</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <?php include 'header.html'; ?>
+    <div class="main-container">
+        <h1>Tinklaraštis</h1>
+        <main>
+            <article class="tinklarastis-articles-container"><a href="tinklarastis_kaip_nesuklysti_renkantis_vestuviu_fotografa.php" class="link-to-article">
+                <h3 class="article-title">7 išsamūs patarimai kaip nesuklysti renkantis vestuvių fotografą</h3>
+                <div class="inner-article-container">
+                    <img class="article-image" src="../images/tinklarastis/issamus_patarimai_kaip_nesuklysti_renkantis_vestuviu_fotografa.JPG" alt="Jaunikis ir nuotaka">
+                    <div class="article-intro">Įvairių apklausų duomenimis vestuvių fotografai patenka tarp TOP 5 pirmiausia pasirenkamų vestuvinių paslaugų tiekėjų kartu su vestuvių planuotoja, ceremonijos vieta, šventės vieta ir vestuvių vedėju (DJ, grupe). Nuo šito penketuko rezervacijų ir reikėtų pradėti planuojant savo vestuves, kadangi geriausių savo srities specialistų darbų sąrašas neretai būna pildomas metams į priekį, o ...</div>
+                </div></a>
+            </article>
+            <article class="tinklarastis-articles-container"><a href="tinklarastis_vestuviu_fotografavimo_vietos_klaipedoje_ir_klaipedos_apskrityje.php" class="link-to-article">
+                <h3 class="article-title">25+ patikrintos vestuvių fotografavimo vietos Klaipėdoje ir Klaipėdos apskrityje</h3>
+                <div class="inner-article-container">
+                    <img class="article-image" src="../images/tinklarastis/Graziausios_geriausios_vietos_vestuviu_fotosesijai_Klaipedoje_Klaipedos_rajone.jpg" alt="Bažnyčia saulėlydžio metu">
+                    <p class="article-intro">Planuojant vestuves neišvengiamai kyla klausimas kur fotografuotis per vestuves? Vestuvių fotosesiją pirmiausia siūlome daryti tose vietose kurios Jums turi reikšmę ir apie kurias pagalvojus Jus užplūsta šilti prisiminimai. Jus vienijanti veikla ar hobis, gali būti puiki idėja ir taip pat pasufleruoti vietą vestuvinei fotosesijai. Antras pasiūlymas klausti patarimo pas ...</p>
+                </div></a>
+            </article>
+        </main>
+    </div>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
