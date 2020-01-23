f8d6d4c6ef1ce45f3a5287985c9f2507162e30ad
GSkiezgilas <giedriusskiezgilas@yahoo.com>
2020-01-23 20:30:53 +0200

    tinklarastis subpage added

---
+++ pages/tinklarastis_vestuviu_fotografavimo_vietos_klaipedoje_ir_klaipedos_apskrityje.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>25+ vestuvių fotografavimo vietos Klaipėdoje ir apskrityje | Kronos Portraits</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <?php include 'header.html'; ?>
+    <main>
+        <h1>25+ patikrintos vestuvių fotografavimo vietos Klaipėdoje ir jos apskrityje</h1>
+        <article class="article-container">
+            <p class="blog-article-paragraph">Planuojant vestuves neišvengiamai kyla klausimas kur fotografuotis per vestuves? Vestuvių fotosesiją pirmiausia siūlome daryti tose vietose kurios Jums turi reikšmę ir apie kurias pagalvojus Jus užplūsta šilti prisiminimai. Jus vienijanti veikla ar hobis, gali būti puiki idėja ir taip pat pasufleruoti vietą vestuvinei fotosesijai. Antras pasiūlymas klausti patarimo pas savo fotografą (kaip išsirinkti geriausią vestuvių fotografą rašėme <a href="tinklarastis_kaip_nesuklysti_renkantis_vestuviu_fotografa.php">čia</a>), o jei jis padėti negali, nes kai rinkotės dar nebuvote matę mūsų įrašo, padėsime mes.</p>
+            <div class="article-image-container-width"><img src="../images/tinklarastis/Graziausios_geriausios_vietos_vestuviu_fotosesijai_Klaipedoje_Klaipedos_rajone.jpg" alt="Bažnyčia saulėlydžio metu" >
+            </div>
+            <p class="blog-article-paragraph">Taigi, mūsų nuomone, gražiausios ir geriausios vietos vestuvių fotosesijoms Klaipėdoje ir jos apskrityje yra šios:</p>
+            <div class="article-li-image-container">
+                <ul>
+                    <li class="article-h3-li-style"><h3 class="article-h3-style">Klaipėdoje ir iki 20km aplinkui:</h3>
+                        <ul>
+                           <li>Senamiestis</li>
+                            <li>Klaipėdos universiteto miestelis</li>
+                            <li>Molas</li>
+                            <li>Klaipėdos universiteto botanikos sodas</li>
+                            <li>Olando kepurė</li>
+                            <li>Pakrančių apsaugos baterija</li>
+                            <li>Dinozaurų parkas</li>
+                            <li>Klaipėdos viešbutis, restoranai XII ir VIVA LAVITA</li>
+                            <li>Viešbutis "Ararat"</li>
+                            <li>Klaipėdos orouostas</li>
+                            <li>Drevernos apžvalgos bokštas ir uostelis</li>
+                        </ul>
+                    </li>
+                    <li class="article-h3-li-style"><h3 class="article-h3-style">Palangoje ir iki 20 km aplinkui:</h3>
+                        <ul>
+                            <li>Pajurys</li>
+                            <li>Tiškevičių rūmai ir Birutės parkas</li>
+                            <li>Zigmo žirgynas</li>
+                            <li>HBH pas Juozapą</li>
+                            <li>Kretingos muziejus ir žiemos sodas</li>
+                            <li>Golfo aikštynas "National golf resort"</li>
+                        </ul>
+                    </li>
+                    <li class="article-h3-li-style"><h3 class="article-h3-style">Šventojoje ir iki 20 km aplinkui:</h3>
+                        <ul>
+                            <li>Pajūris</li>
+                            <li>Beždžionių tiltas</li>
+                            <li>Lazdininkų vėjo malūnas</li>
+                            <li>Japoniškas sodas</li>
+                        </ul>
+                    </li>
+                    <li class="article-h3-li-style"><h3 class="article-h3-style">Gargžduose ir iki 20km aplinkui:</h3>
+                        <ul>
+                            <li>Kalniškės piliakalnis</li>
+                        </ul>
+                    </li>
+                    <li class="article-h3-li-style"><h3 class="article-h3-style">Nidoje, Juodkrantėje ir iki 20km aplinkui:</h3>
+                        <ul>
+                            <li>Saulės laikrodis</li>
+                            <li>Parnidžio kopa</li>
+                            <li>Nidos švyturys</li>
+                        </ul>
+                    </li>
+                    <li class="article-h3-li-style"><h3 class="article-h3-style">Švėkšnoje ir iki 20km aplinkui:</h3>
+                        <ul>
+                            <li>Švėkšnos parkas ir dvaras</li>
+                            <li>Vilkėnų malūnas ir dvaras (apleistas)</li>
+                            <li>Vyndario sodyba</li>
+                            <li>Saugų malūnas</li>
+                            <li>Vėjo jėgainės</li>
+                        </ul>
+                    </li>
+                    <li class="article-h3-li-style"><h3 class="article-h3-style">Šilutėje ir iki 20 km aplinkui:</h3>
+                        <ul>
+                            <li>Hugo Šojaus dvaras</li>
+                            <li>Akmeninis geležinkelio tiltas</li>
+                            <li>Ventės ragas</li>
+                            <li>Mingės kaimas</li>
+                            <li>Šilutės orouostas</li><li>Uostadvaris</li>
+                            <li>Rusnės beždžionių tiltas</li>
+                            <li>Rusnės uostelis</li>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+            <p class="blog-article-paragraph">Turėkite omenyje kad kai kurios iš išvardintų vietų gali būti mokamos, sezono metu jose gali būti daug žmonių, gali būti užsakyta kitų asmenų, ar gamtos stichija ją gali padaryti neprieinamą. Susižinokite iš anksto reikalingą informaciją, kad atvykus į vietą netektų nusivylti.</p>
+            <p class="blog-article-paragraph">Rinkdamiesi vietas fotografavimuisi įsitraukit ir tokių kurias galėtumėt išnaudoti jei gamta iškrėstų Jums pokštą ir vestuvių dieną imtų lyti lietus, bet jei gamtos pokštai Jums tik kelią juoką, tada pirmyn taškytis į balas, liepsnojančiai meilei jokie lietūs nebaisūs.</p>
+            <p class="blog-article-paragraph">Sėkmės planuojant ir gražių Jums šventės akimirkų!</p>
+            <p class="blog-article-paragraph">P.S. Visas mūsų naujienas greičiausiai sužinosite sekdami <a href="https://www.facebook.com/KronosPortraits/" target="_blank">mūsų Facebook paskyrą</a>. Jei turit klausimų, pasiūlymų naujiems straipsniams ar norite pasidalinti savo patirtimi ar įžvalgomis nesikuklinkite ir susisiekite su mumis <a href="kontaktai.php">čia</a>.
+            </p>
+        </article>
+    </main>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
